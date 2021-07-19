22279ed31c06ff78117205048bdf577304f5c1f3
Richard.C <r.conde@live.com>
2021-07-19 10:10:04 -0400

    toggle method added to rules-area div

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
 // toggle hide and show for instructions btn
     $("#instructions").on("click",function(){
-        console.log("clicked");
+        // console.log("clicked");
+        $("#rules-area").toggle();
     });
 });
