82b67d4cedfe49532528389330b95c55576dae51
Richard.C <r.conde@live.com>
2021-07-19 10:50:36 -0400

    function to get random numbers for any variable

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,41 +1,41 @@
-$(document).ready(function(){
-// toggle hide and show for instructions btn
-    $("#instructions").on("click",function(){
-        // console.log("clicked");
-        $("#rules-area").toggle(3000);
-    });
+$(document).ready(function () {
+  // toggle hide and show for instructions btn
+  $("#instructions").on("click", function () {
+    // console.log("clicked");
+    $("#rules-area").toggle(3000);
+  });
 
- // Crystal Variables
-var crystal = {
-    blue:
-    {
+  // Crystal Variables
+  var crystal = {
+    blue: {
       name: "Blue",
-      value: 0
+      value: 0,
     },
-    green:
-    {
+    green: {
       name: "Green",
-      value: 0
+      value: 0,
     },
-    red:
-    {
+    red: {
       name: "Red",
-      value: 0
+      value: 0,
     },
-    yellow:
-    {
+    yellow: {
       name: "Yellow",
-      value: 0
-    }
+      value: 0,
+    },
   };
-  
+
   // Scores (Current and Target)
   var currentScore = 0;
   var targetScore = 0;
-  
+
   // Wins and Losses
   var winCount = 0;
   var lossCount = 0;
 
-  
+  // Functions
+  // Helper Function for getting random numbers
+  var getRandom = function (min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
 });
